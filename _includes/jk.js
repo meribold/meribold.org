9da6330dfe9b5956e4816c6e2d43627f6badb105
Lukas Waymann <git@meribold.org>
2023-12-04 16:27:30 +0100

    Add more keybinds to/in jk.js

--- _includes/jk.js
+++ _includes/jk.js
@@ -2,6 +2,9 @@ addEventListener("keydown", (e) => {
    if (e.isComposing) return;
    if (e.key === "j") scrollBy({ top: 53 });
    if (e.key === "k") scrollBy({ top: -53 });
+   if (e.key === "d") scrollBy({ top: innerHeight / 2 });
+   if (e.key === "u") scrollBy({ top: -innerHeight / 2 });
    if (e.key === "G") scroll({ top: document.body.scrollHeight - 1.35 * innerHeight + 4 });
    if (e.key === "g") scroll({ top: 0 });
+   if (e.key === "t" || e.key === "i") themeToggle.click();
 });
